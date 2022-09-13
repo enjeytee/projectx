f87d233c5540a549008a476272f9124fa479c89d
enjeytee <enjeytee@gmail.com>
2022-09-13 20:15:47 +0800

    update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,9 @@
 
 export default function SearchBar() {
     return (
-        <div>
-            <input type="text" placeholder="Search transaction"/>
-            <button>Search</button>
+        <div className="container-search">
+            <input className="input-search" type="text" placeholder="Search transaction"/>
+            <a className="button-search">Search</a>
         </div>
     );
 };
--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -2,6 +2,21 @@
 
 export default function Transactions() {
     return (
-        <p>This is where the transactions will show.</p>
+        <div className="transactions">
+            <h6>D.R. NUMBER</h6>
+            <h6>STATUS</h6>
+            <h6>SUPPLIER</h6>
+            <h6>P.O. NUMBER</h6>
+            <h6>SKU TOTAL</h6>
+            <h6>DELIVERED QUANTITY</h6>
+            <h6>ACCEPTED QUANTITY</h6>
+            <h6>DELIVERED AMOUNT</h6>
+            <h6>ACCEPTED AMOUNT</h6>
+            <h6>TYPE</h6>
+            <h6>DELIVERY DATE</h6>
+            <h6>ETA</h6>
+            <h6>DATE CREATED</h6>
+            <h6>ACTIONS</h6>
+        </div>
     );
 };
