ec2232a1ceeb28232e08c0d9c887a222c05195e6
enjeytee <enjeytee@gmail.com>
2022-09-15 14:08:51 +0800

    update css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import myLogo from "../images/my-logo.png";
 const Footer = () => {
     return (
         <footer className="footer">
-            <a href="https://trajanonj-portfolio.netlify.app" target="_blank"><img className="my-logo" src={myLogo} width="85rem"/></a>
+            <a href="https://trajanonj-portfolio.netlify.app" target="_blank"><img className="my-logo" src={myLogo}/></a>
         </footer>
     );
 };
